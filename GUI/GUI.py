715b6c9785ddbbdb63d41f7fa90ae5aa9386aac3
Karen <ksuzue22@wooster.edu>
2021-04-19 15:33:28 +0700

    GUI done

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,5 @@
 from tkinter import *
-from PIL import Image
-from PIL import ImageDraw
+from PIL import Image, ImageTk
 
 from grid import Grid
 from image_export.to_png import ToPNG
@@ -17,15 +16,31 @@ WIDTH = 700
 GRID_ROW = 30
 GRID_COL = 20
 
+CELL_SIZE = 20
+
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("Maze Generator")
 
+        # Header
+        self.header = Label(self.master, text="Maze Generator", padx= 10, pady=5, font=10)
+        self.header.pack()
+
+        # Instruction
+        self.instruction = Label(self.master, text="Before generating a new maze, click RESET", fg="#f00", padx= 5)
+        self.instruction.pack()
+
+        # Menu frame
+        self.menu = Frame(self.master)
+        self.menu.pack()
+
         # Initialize grid
         self.grid = Grid(GRID_ROW, GRID_COL)
 
         # Options for maze generation algorithms
+        self.algo_label = Label(self.menu, text="Maze generation algorithms:")
+        self.algo_label.pack()
         self.algorithms = {"Binary Tree": 1,
                            "Sidewinder": 2,
                            "Kruskal's": 3,
@@ -35,16 +50,16 @@ class GUI:
         self.v_algorithm = IntVar()
         self.v_algorithm.set(1)
         for (text, value) in self.algorithms.items():
-            Radiobutton(master, text=text, variable=self.v_algorithm,
+            Radiobutton(self.menu, text=text, variable=self.v_algorithm,
                         value=value).pack()
 
         # Plain maze or colored bias map
-        self.map_label = Label(master, text="Maze rendering options:")
+        self.map_label = Label(self.menu, text="Maze rendering options:")
         self.v_render = IntVar()
         self.v_render.set(1)
-        self.plain_option = Radiobutton(master, text="Plain Maze",
+        self.plain_option = Radiobutton(self.menu, text="Plain Maze",
                                         variable=self.v_render, value=1)
-        self.color_option = Radiobutton(master, text="Colored Maze",
+        self.color_option = Radiobutton(self.menu, text="Colored Maze",
                                         variable=self.v_render, value=2)
 
         self.map_label.pack()
@@ -52,13 +67,21 @@ class GUI:
         self.color_option.pack()
 
         # Generate button
-        self.generate_button = Button(master, text="Generate Maze",
+        self.generate_button = Button(self.menu, text="Generate Maze",
                                       command=self.generate)
         self.generate_button.pack()
 
         # Reset button
-        self.reset_button = Button(master, text="Reset", command=self.reset)
+        self.reset_button = Button(self.menu, text="Reset", command=self.reset)
         self.reset_button.pack()
+
+    def image_load(self):
+        if self.v_render.get() == 2:
+            image = Image.open("maze-colored.png")
+            return ImageTk.PhotoImage(image)
+        else:
+            image = Image.open("maze.png")
+            return ImageTk.PhotoImage(image)
 
     def reset(self):
         self.grid = Grid(GRID_ROW, GRID_COL)
@@ -89,7 +112,7 @@ class GUI:
 
         # Render choice
         print(self.grid)
-        render = ToPNG(self.grid, 20)
+        render = ToPNG(self.grid, CELL_SIZE)
 
         if self.v_render.get() == 1:
             render.render()
@@ -97,8 +120,20 @@ class GUI:
         if self.v_render.get() == 2:
             render.render_color()
 
+        # New window to display generated maze
+        new_window = Toplevel(self.master)
+        new_window.title("Display Maze")
+
+        # Maze image
+        img = self.image_load()
+        img_label = Label(new_window, image=img)
+        img_label.image = img
+        img_label.place(x=10, y=10)
+        img_label.pack()
+
 
 if __name__ == '__main__':
     root = Tk()
+    # root.geometry("700x500")
     my_gui = GUI(root)
     root.mainloop()
