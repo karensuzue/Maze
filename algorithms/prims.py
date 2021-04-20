f360f1dfb8951c90c36c9135630f848b390ab28c
Karen <ksuzue22@wooster.edu>
2021-04-20 23:02:59 +0700

    prim draft

--- algorithms/prims.py
+++ algorithms/prims.py
@@ -3,7 +3,7 @@ from grid import Grid
 from grid import Cell
 
 class Prim():
-    def find_neighbors(self, cell, grid):
+    def find_unvisited_neighbors(self, cell, grid):
         # Find available neighbors
         neighbors = []
         north = grid.get_north(cell)
@@ -41,9 +41,66 @@ class Prim():
         return minimum
 
     def generate(self, grid):
+        first = grid.random_cell()
+        path = [first]
+        while len(path) > 0:
+            cell = random.choice(path)
+            cell.visited = True
+
+            visited_neighbors =
+            available_neighbors = self.find_neighbors(cell, grid)
+            if len(available_neighbors) > 0:
+                neighbor = random.choice(available_neighbors)
+                cell.link(neighbor)
+
+                neighbors_of_neighbor = self.find_neighbors(neighbor, grid)
+                for i in range(len(neighbors_of_neighbor)):
+                    if neighbors_of_neighbor[i] not in path:
+                            path.append(neighbors_of_neighbor[i])
+
+
+            path.remove(cell)
+
+
+
+
+
+
+
+
+
+    def generate3(self, grid):
         # Arbitrary starting cell
         first = grid.random_cell()
-        active = [first]
+        frontier = [first]
+        maze = []
+
+        while len(frontier) > 0:
+            cell = random.choice(frontier)
+            maze.append(cell)
+            frontier.remove(cell)
+            available_neighbors = self.find_neighbors(cell, grid)
+            print(available_neighbors)
+
+            # Avoid duplicates in frontier set
+            for i in range(len(available_neighbors)):
+                if available_neighbors[i] not in (frontier or maze):
+                    frontier.append(available_neighbors[i])
+
+            if len(available_neighbors) > 0:
+                for i in range(len(maze)):
+                    if maze[i] in available_neighbors:
+                        cell.link(maze[i])
+                        break
+
+
+
+
+    def generate2(self, grid):
+        # Arbitrary starting cell
+        first = grid.random_cell()
+        frontier = [first]
+        maze = []
 
         # Assign random costs to each cell (not edges)
         costs = {}
@@ -52,25 +109,18 @@ class Prim():
                 cell = grid.grid[r][c]
                 costs[cell] = random.randint(1, 100)
 
-        while len(active) > 0:
-            cell = self.min_element(active, costs)  # Cell with minimum cost
-            print(cell)
+        while len(frontier) > 0:
+            cell = self.min_element(frontier, costs)  # Cell with minimum cost
+            maze.append(cell)
             available_neighbors = self.find_neighbors(cell, grid)
 
-            # Avoid duplicates
+            # Avoid duplicates in frontier set
             for i in range(len(available_neighbors)):
-                if available_neighbors[i] not in active:
-                    active.append(available_neighbors[i])
+                if available_neighbors[i] not in (frontier and maze):
+                    frontier.append(available_neighbors[i])
 
-            if len(available_neighbors) > 0:
-                neighbor = self.min_element(active, costs)
-                cell.link(neighbor)
-                active.remove(neighbor)
-                active.extend(self.find_neighbors(neighbor, grid))
-
-
-            else:
-                active.remove(cell)
-
-            #print(active)
-
+            for i in range(len(maze)):
+                if maze[i] in available_neighbors:
+                    cell.link(maze[i])
+                    frontier.remove(cell)
+            print(frontier)
